44a5b23d7ce7ab5cde14eb6d98a01c5c3d4e417d
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-11-12 19:17:49 -0500

    ...

--- parse.js
+++ parse.js
@@ -1,11 +1,27 @@
 //const parsedMap = new WeakMap<HTMLTemplateElement, Map<string, any>>();
-export function parse(templ) {
+export async function parse(templ) {
     const map = new Map();
-    const countCounter = {
+    const countContainer = {
         count: 0
     };
-    //parsedMap.set(templ, map);
-    parseElements(templ.content.children, map, count);
+    parseElements(templ.content.children, map, countContainer);
+    if (countContainer.count > 0) {
+        const { lispToCamel } = await import('trans-render/lib/lispToCamel.js');
+        const existingClone = templ.content.cloneNode;
+        existingClone.bind(templ.content);
+        templ.content.cloneNode = (deep) => {
+            const clone = existingClone(deep);
+            for (const className of map.keys()) {
+                const target = clone.querySelector('.' + className);
+                const parseObj = map.get(className);
+                const key = lispToCamel(parseObj.key.replace('be-', ''));
+                if (target.beDecorated === undefined)
+                    target.beDecorated = {};
+                target.beDecorated.key = parseObj.parsed;
+            }
+            return clone;
+        };
+    }
 }
 function parseElements(children, map, countContainer) {
     const arr = Array.from(children);
@@ -30,5 +46,6 @@ function parseElements(children, map, countContainer) {
                 catch (e) { }
             }
         }
+        parseElements(child.children, map, countContainer);
     }
 }
--- parse.ts
+++ parse.ts
@@ -1,11 +1,26 @@
 //const parsedMap = new WeakMap<HTMLTemplateElement, Map<string, any>>();
-export function parse(templ: HTMLTemplateElement){
+export async function parse(templ: HTMLTemplateElement){
     const map = new Map<string, ParseObj>();
-    const countCounter = {
+    const countContainer = {
         count: 0
     } as CountContainer;
-    //parsedMap.set(templ, map);
-    parseElements(templ.content.children, map, count);
+    parseElements(templ.content.children, map, countContainer);
+    if(countContainer.count > 0){
+        const {lispToCamel} = await import('trans-render/lib/lispToCamel.js') 
+        const existingClone = templ.content.cloneNode;
+        existingClone.bind(templ.content);
+        templ.content.cloneNode = (deep?: boolean | undefined) => {
+            const clone =  existingClone(deep) as DocumentFragment;
+            for(const className of map.keys()){
+                const target = clone.querySelector('.' + className) as any;
+                const parseObj = map.get(className);
+                const key = lispToCamel(parseObj!.key.replace('be-', ''));
+                if(target.beDecorated === undefined) target.beDecorated = {};
+                target.beDecorated.key = parseObj!.parsed;
+            }
+            return clone;
+        }
+    }
 }
 
 function parseElements(children: HTMLCollection, map: Map<string, ParseObj>, countContainer: CountContainer){
@@ -28,11 +43,10 @@ function parseElements(children: HTMLCollection, map: Map<string, ParseObj>, cou
                     countContainer.count++;
                     child.classList.add(className);
                     map.set(className, parseObj);
-
                 }catch(e){}
             }
         }
-
+        parseElements(child.children, map, countContainer);
     }
 }
 
